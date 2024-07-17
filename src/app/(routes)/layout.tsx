befe6b1975d69d86a828e2ef6363e488477eed38
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-17 23:16:00 +0300

    refactor: Optimize layout component styles and add responsive behavior

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -3,10 +3,10 @@ import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full relative">
+    <div className="h-screen relative bg-gray-100">
       <div
-        className="hidden h-full md:flex md:w-72 md:flex-col md:fixed 
-      md:inset-y-0 z-[80] bg-gray-900"
+        className="hidden h-full w-[252px] md:flex  md:flex-col md:fixed 
+      md:inset-y-0 z-[80]"
       >
         <Sidebar />
       </div>
