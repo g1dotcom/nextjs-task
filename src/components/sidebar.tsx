733e01c748b7e79c4062b16c43fdc98ff6aa319b
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-17 23:15:53 +0300

    refactor: Optimize Sidebar component and remove unused imports

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      sidebar
+    <div className="space-y-4 py-4 flex flex-col h-full text-white bg-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-[188px] h-[40px] mr-4">
+            <Image fill alt="Logo" src="/logo.png" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
