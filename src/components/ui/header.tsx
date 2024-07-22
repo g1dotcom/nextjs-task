29d2b7fadc7d77aff4050c50f6f65e3fa21a45f8
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-22 19:31:26 +0300

    refactor: Add Header component to UI folder

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,10 @@
+// Header.tsx
+const Header = ({ widthClass }: { widthClass: string }) => {
+  return (
+    <header className={`bg-red-500 shadow ${widthClass}`}>
+      {/* Header içeriği */}dfkgjdsflkghldfjkh
+    </header>
+  );
+};
+
+export default Header;
