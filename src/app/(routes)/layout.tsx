8c392550145776ded3015f8d46cdd553befa2b5b
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-22 19:31:14 +0300

    refactor: Update layout component with lazy loading for images

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,14 +1,22 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import Header from "@/components/ui/header";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const isOverviewPage = pathname === "/overview"; // "/overview" yolunu kendi yolunuzla değiştirin
+
+  const headerWidthClass = isOverviewPage ? "w-[1004px]" : "w-full";
+
   return (
     <div className="h-screen relative bg-[#FAFAFA]">
-      <div
-        className="hidden h-full w-[252px] md:flex  md:flex-col md:fixed 
-      md:inset-y-0 z-[80]"
-      >
+      <div className="hidden h-full w-[252px] md:flex md:flex-col md:fixed md:inset-y-0 z-[80]">
         <Sidebar />
+      </div>
+      <div className="w-full h-44 ">
+        <Header widthClass={headerWidthClass} />
       </div>
       <main className="md:pl-72">
         <Navbar />
@@ -18,4 +26,4 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default DashboardLayout;
+export default Layout;
