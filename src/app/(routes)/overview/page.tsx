8c891b0e4eeb7c4e24889f4141a94a35747b6fd1
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-22 20:28:14 +0300

    refactor: Update overview page header with user information

--- src/app/(routes)/overview/page.tsx
+++ src/app/(routes)/overview/page.tsx
@@ -2,8 +2,18 @@ import OverviewComponent from ".";
 
 const Overview = () => {
   return (
-    <div className="w-full h-screen flex flex-col bg-green-500">
-      <OverviewComponent />
+    <div className="w-full h-screen flex flex-col bg-[#FAFAFA] ">
+      <header className="bg-[#FFFFFF] h-[192px] flex justify-between p-8">
+        <div className="">
+          <h1 className="text-secondinary-500 text-[24px] font-semibold">
+            Hi,Skylar Dias
+          </h1>
+          <p className="text-secondinary-400 text-[16px] font-medium">
+            Lets finish your task today!
+          </p>
+        </div>
+        <div className=""></div>
+      </header>
     </div>
   );
 };
