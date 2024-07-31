9d7cb52c4a38a62a570bd2dee6c7d40dbe82d0a9
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-31 21:07:32 +0300

    refactor: Update MonthlyMentors component in overview page

--- src/components/overview/MonthlyMentors.tsx
+++ src/components/overview/MonthlyMentors.tsx
@@ -30,7 +30,26 @@ function MonthlyMentors() {
             <Notepad2 color="#54577A" size={24} /> 40 Task
           </h1>
         </div>
-        <div className="bg-primaryy-0 w-[328px] h-[140px]">dfdsfds</div>
+        <div className="bg-primaryy-0 w-[328px] h-[140px]  flex flex-col  ">
+          <div className="flex justify-between items-center ">
+            <Avatar>
+              <AvatarImage src="/profil.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col w-[124px] h-[40px]">
+              <h1 className="text-secondinary-500 font-semibold text-[16px]">
+                Curious George
+              </h1>
+              <p className="text-secondinary-400 font-semibold text-[16px]">
+                UI UX Design
+              </p>
+            </div>
+            <Button variant="follow">+Follow</Button>
+          </div>
+          <h1 className="flex text-secondinary-500 font-medium">
+            <Notepad2 color="#54577A" size={24} /> 40 Task
+          </h1>
+        </div>
       </div>
     </div>
   );
