eada4f6fb14fc96f2e4fa35835bec4761d1fb891
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-31 21:02:53 +0300

    refactor: Update MonthlyMentors component in overview page

--- src/components/overview/MonthlyMentors.tsx
+++ src/components/overview/MonthlyMentors.tsx
@@ -1,3 +1,4 @@
+import { Notepad2 } from "iconsax-react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 
@@ -9,20 +10,25 @@ function MonthlyMentors() {
       </h1>
 
       <div className="flex rounded-[10px] gap-[32px] ">
-        <div className="bg-primaryy-0 w-[328px] h-[140px] flex justify-between items-center ">
-          <Avatar>
-            <AvatarImage src="/profil.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col w-[124px] h-[40px]">
-            <h1 className="text-secondinary-500 font-semibold text-[16px]">
-              Curious George
-            </h1>
-            <p className="text-secondinary-400 font-semibold text-[16px]">
-              UI UX Design
-            </p>
+        <div className="bg-primaryy-0 w-[328px] h-[140px]  ">
+          <div className="flex justify-between items-center ">
+            <Avatar>
+              <AvatarImage src="/profil.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col w-[124px] h-[40px]">
+              <h1 className="text-secondinary-500 font-semibold text-[16px]">
+                Curious George
+              </h1>
+              <p className="text-secondinary-400 font-semibold text-[16px]">
+                UI UX Design
+              </p>
+            </div>
+            <Button variant="follow">+Follow</Button>
           </div>
-          <Button variant="follow">+Follow</Button>
+          <h1 className="flex text-secondinary-500 font-medium">
+            <Notepad2 color="#54577A" size={24} /> 40 Task
+          </h1>
         </div>
         <div className="bg-primaryy-0 w-[328px] h-[140px]">dfdsfds</div>
       </div>
