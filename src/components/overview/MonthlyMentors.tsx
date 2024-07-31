ebb7831ba085e5c6d2d0540ca7fb447e6e226ecf
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-31 20:17:55 +0300

    refactor: Update MonthlyMentors component in overview page

--- src/components/overview/MonthlyMentors.tsx
+++ src/components/overview/MonthlyMentors.tsx
@@ -1,11 +1,14 @@
 function MonthlyMentors() {
   return (
-    <div className="">
+    <div className="flex flex-col">
       <h1 className="text-secondinary-500 text-[24px] font-semibold">
         Monthly Mentors
       </h1>
-      <div className="bg-primaryy-0 w-[328px] h-[140px]">dfdsfds</div>
-      <div className=""></div>
+
+      <div className="flex rounded-[10px] gap-[32px]">
+        <div className="bg-primaryy-0 w-[328px] h-[140px]">dfdsfds</div>
+        <div className="bg-primaryy-0 w-[328px] h-[140px]">dfdsfds</div>
+      </div>
     </div>
   );
 }
