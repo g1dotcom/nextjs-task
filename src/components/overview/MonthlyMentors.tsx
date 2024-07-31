80a1d4eb63a1a1c8160fab587a5cf917932f9665
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-31 19:38:37 +0300

    refactor: Add MonthlyMentors component to overview page

---
+++ src/components/overview/MonthlyMentors.tsx
@@ -0,0 +1,13 @@
+function MonthlyMentors() {
+  return (
+    <div className="">
+      <h1 className="text-secondinary-500 text-[24px] font-semibold">
+        Monthly Mentors
+      </h1>
+      <div className="bg-primaryy-0">dfdsfds</div>
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default MonthlyMentors;
