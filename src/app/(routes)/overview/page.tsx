efd252dc67bcaf611dd477d57bce683b735b4a76
Gökhan Süle <thegkhnsl@gmail.com>
2024-07-31 18:00:11 +0300

    refactor: Update overview page with CircleBar and ActivityChart components

--- src/app/(routes)/overview/page.tsx
+++ src/app/(routes)/overview/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CircleBar from "@/components/overview/CircleBar";
 import { ActivityChart } from "@/components/overview/taskToday";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Notification } from "iconsax-react";
@@ -7,7 +8,6 @@ import { Notification } from "iconsax-react";
 import { CircularProgressbar } from "react-circular-progressbar";
 
 const Overview = () => {
-  const value = 0.45;
   return (
     <div className="w-full h-screen flex flex-col bg-[#FAFAFA] ">
       <header className="bg-[#FFFFFF] h-[192px] flex justify-between p-8">
@@ -29,36 +29,8 @@ const Overview = () => {
         </div>
       </header>
       <div className="w-full flex justify-between items-start p-8">
-        <div className="w-[194px] h-[214px] bg-secondinary-500 text-white rounded-[10px] p-4 flex flex-col space-y-5">
-          <h1 className="font-semibold text-[16px] text-[#ffffff]">
-            Running Task
-          </h1>
-          <p className="font-semibold text-[32px]">65</p>
-          <div className="flex  ">
-            <CircularProgressbar
-              value={value}
-              maxValue={1}
-              text={`${value * 100}%`}
-              className=" h-[68px]"
-              strokeWidth={4}
-              styles={{
-                path: { stroke: "#546FFF" },
-                trail: { stroke: "#1a1e38" },
-                text: { fill: "white", fontSize: "18px", fontWeight: "normal" },
-              }}
-            />
-
-            <h1 className="flex flex-col w-40 justify-center ">
-              <span className="font-semibold text-[20px] ml-4">100</span>
-              <span className="font-medium text-[14px] text-secondinary-300 ml-4">
-                Task
-              </span>
-            </h1>
-          </div>
-        </div>
-        <div className="w-[462px] h-[214px]">
-          <ActivityChart />
-        </div>
+        <CircleBar />
+        <ActivityChart />
       </div>
     </div>
   );
