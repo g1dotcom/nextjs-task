2019ffd7c49146a8b401abae55bd1c5023088549
Gökhan Süle <thegkhnsl@gmail.com>
2024-08-06 21:18:15 +0300

    refactor: Update layout component to use min-height for better responsiveness

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -6,12 +6,12 @@ import "react-circular-progressbar/dist/styles.css";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen relative bg-[#FAFAFA]">
+    <div className="min-h-screen relative  bg-[#FAFAFA]">
       <div className="hidden h-full w-[252px] md:flex md:flex-col md:fixed md:inset-y-0 z-[80]">
         <Sidebar />
       </div>
 
-      <main className=" md:pl-[252px]">
+      <main className="h-full md:pl-[252px]">
         <Navbar />
         {children}
       </main>
