1797c9388e973b19ff7696aeade6a777b743800a
Gökhan Süle <thegkhnsl@gmail.com>
2024-08-06 19:30:03 +0300

    feat: Update overview page with MontlyCarousel and UpcomingTask components

--- src/app/(routes)/overview/page.tsx
+++ src/app/(routes)/overview/page.tsx
@@ -2,45 +2,49 @@
 
 import { ActivityChart } from "@/components/overview/ActivityCharts";
 import CircleBar from "@/components/overview/CircleBar";
-import MonthlyMentors from "@/components/overview/MonthlyMentors";
+import { MontlyCarousel } from "@/components/overview/MontlyCarousel";
+import { UpcomingTask } from "@/components/overview/upcomingTask";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { UpcomingTask } from "@/components/ui/upcomingTask";
+
 import { Notification } from "iconsax-react";
 
 const Overview = () => {
   return (
-    <div className="w-3/5 h-screen flex flex-col bg-[#FAFAFA] ">
-      <header className="bg-[#FFFFFF] h-[192px] flex justify-between p-8">
-        <div className="w-[236px] h-[72px] flex flex-col items-start justify-center ">
-          <h1 className="text-secondinary-500 text-[24px] font-semibold">
-            Hi,Skylar Dias
-          </h1>
-          <p className="text-secondinary-400 text-[16px] font-medium">
-            Lets finish your task today!
-          </p>
-        </div>
-        <div className="w-[128px] h-[52px] flex justify-between items-center ">
-          <Notification color="#8E92BC" variant="Outline" size={24} />
+    <div className="w-[1440px] h-screen flex   bg-red-300">
+      <div className="w-[752px] h-screen flex flex-col bg-[#FAFAFA]">
+        <header className="bg-[#FFFFFF] w-full h-[192px] flex justify-between p-8">
+          <div className="w-[236px] h-[72px] flex flex-col items-start justify-center ">
+            <h1 className="text-secondinary-500 text-[24px] font-semibold">
+              Hi,Skylar Dias
+            </h1>
+            <p className="text-secondinary-400 text-[16px] font-medium">
+              Lets finish your task today!
+            </p>
+          </div>
+          <div className="w-[128px] h-[52px] flex justify-between items-center ">
+            <Notification color="#8E92BC" variant="Outline" size={24} />
 
-          <Avatar>
-            <AvatarImage src="/profil.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-      </header>
-      <div className="w-full h-full flex flex-col">
-        <div className="w-full flex justify-between items-start p-8">
-          <CircleBar />
-          <ActivityChart />
-        </div>
-        <div className="w-full flex justify-between items-start p-8">
-          <MonthlyMentors />
-        </div>
-        <div className="w-full flex justify-between items-start p-8">
-          <UpcomingTask />
+            <Avatar>
+              <AvatarImage src="/profil.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+        <div className="w-full h-full flex flex-col">
+          <div className="w-full flex space-x-12 items-start p-8">
+            <CircleBar />
+            <ActivityChart />
+          </div>
+          <div className="w-full p-8 ">
+            <MontlyCarousel />
+          </div>
+          <div className="w-full p-8 ">
+            <UpcomingTask />
+          </div>
         </div>
       </div>
+      <div className="w-[436px] h-screen bg-green-500">fhfsdfsdfsdsd</div>
     </div>
   );
 };
