4a73853583314a5c9f434cfddae902a5a45b5f94
Gökhan Süle <thegkhnsl@gmail.com>
2024-08-06 21:10:22 +0300

    refactor: Update Progress component to fix styling issues and include title

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,13 +12,13 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondinary-500",
+      "relative h-[8px] w-full overflow-hidden rounded-full bg-primaryy-200",
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primaryy-500 transition-all"
+      className="h-[8px] w-full flex-1 bg-primaryy-500 transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
