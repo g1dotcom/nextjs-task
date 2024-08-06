e615e995b31e4d358f4e3931023524944a4f7173
Gökhan Süle <thegkhnsl@gmail.com>
2024-08-06 21:10:19 +0300

    refactor: Update UpcomingTask component to use lazy loading for images

--- src/components/overview/upcomingTask.tsx
+++ src/components/overview/upcomingTask.tsx
@@ -8,8 +8,10 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Progress } from "../ui/progress";
+import Image from "next/image";
 
 export function UpcomingTask() {
+  const progressValue = 85;
   return (
     <Carousel className="flex flex-col gap-5 w-full">
       <div className="flex justify-between w-full ">
@@ -22,9 +24,31 @@ export function UpcomingTask() {
         </div>
       </div>
       <CarouselContent>
-        <CarouselItem className="bg-red-400">
-          {/* <ProgressBar /> */}
-          <Progress value={75} />
+        <CarouselItem className="  ">
+          <div className="flex flex-col gap-2">
+            <Image
+              src="/overview/CreatingPerfectWebsite.png"
+              alt=""
+              width={280}
+              height={110}
+            />
+
+            <div className="flex flex-col">
+              <h1 className="text-secondinary-500 text-[16px] font-Semibold">
+                Creating Perfect Website
+              </h1>
+              <p className="text-secondinary-400 text-[12px] font-normal">
+                Web Developer
+              </p>
+            </div>
+            <div className="flex justify-between w-[280px] text-[16px] font-medium">
+              <h1 className="text-secondinary-500 ">Progress</h1>
+              <p className="text-primaryy-500">{progressValue}%</p>
+            </div>
+            <div className="w-[280px]  flex justify-start">
+              <Progress value={progressValue} />
+            </div>
+          </div>
         </CarouselItem>
       </CarouselContent>
     </Carousel>
